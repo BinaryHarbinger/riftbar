855a74e6c678265aa7ed437fc16eba4da2dd3eda
BinaryHarbinger <halilefeesen@proton.me>
2026-01-01 15:48:43 +0300

    feat(util): Preffer dash if installed

--- src/shared/util.rs
+++ src/shared/util.rs
@@ -1,9 +1,25 @@
+// ============ shared/util.rs ============
+
+use once_cell::sync::Lazy;
+
+// Detect dash if installed as static variable
+static SHELL_NAME: Lazy<String> = Lazy::new(|| {
+    let is_dash_installed = std::path::Path::new("/bin/dash").exists();
+
+    if is_dash_installed {
+        "/bin/dash".to_string()
+    } else {
+        "/bin/sh".to_string()
+    }
+});
+
+// Run Async Shell Commands
 #[inline]
 pub fn run_command_async(action: String) {
     std::thread::spawn(move || {
         let rt = tokio::runtime::Runtime::new().unwrap();
         rt.block_on(async {
-            let _ = tokio::process::Command::new("sh")
+            let _ = tokio::process::Command::new(&*SHELL_NAME)
                 .arg("-c")
                 .arg(action.clone())
                 .output()
@@ -12,6 +28,7 @@ pub fn run_command_async(action: String) {
     });
 }
 
+// Cut strings to given limit
 #[inline]
 pub fn take_chars(s: &str, x: u64) -> &str {
     if x == 0 {
